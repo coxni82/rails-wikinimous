abc44a73af4d71df3cf26e9aed9a37e08808fbb8
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-05-23 17:03:15 +0200

    bad style

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,34 @@
+class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to article_path(@article)
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :details, :completed)
+  end
+
+end
